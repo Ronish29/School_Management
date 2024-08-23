c8c1d30640e4fb6fff697ff110edaad85fd9267d
Ronish29 <ronishparmar2003@gmail.com>
2024-08-23 21:09:45 +0530

    inital_request

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/api',routes);
 
+app.get('/',(req,res)=>{
+    res.json({
+        success : true,
+        message: "Server is running perfectly"
+    });
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
